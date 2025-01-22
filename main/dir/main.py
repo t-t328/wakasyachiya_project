3649f1de7d98dabf4297e4d4f869008ac21e8737
ShoFukuyama <sho.fukuyama0123@gmail.com>
2025-01-22 16:28:41 +0900

    簡単なルーティング

--- main/dir/main.py
+++ main/dir/main.py
@@ -1,8 +1,14 @@
 # 簡単なindexページの作成を行っています。
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "index page"
+    return render_template('index.html')
+
+@app.route("/menu_page")
+def menu_page():
+    return render_template('menu_page.html')
